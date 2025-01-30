dfadc74c6819fde57b1568508917f66e78336209
RafaBonach <rafa.bonach@gmail.com>
2025-01-30 11:05:50 -0300

    Ajustando as funções de realizar compra e cancelar compra

--- ProjetoPixelPoint/src/backend/Compra.java
+++ ProjetoPixelPoint/src/backend/Compra.java
@@ -3,10 +3,12 @@ package backend;
 public class Compra {
     private Jogo jogo;
     private Cliente cliente;
+    private static double saldoOriginal;
     
     public Compra(Jogo jogo, Cliente cliente){
         this.jogo = jogo;
         this.cliente = cliente;
+        saldoOriginal = cliente.getSaldoCarteira();
     }
     
     public double precoJogo(){
@@ -14,14 +16,36 @@ public class Compra {
     }
     
     public double clienteSaldo(){
-        return cliente.getSaldoCarteira(cliente.getSenha());
+        return cliente.getSaldoCarteira();
     }
     
-    public void efetuaCompra(){
-        cliente.subtraiSaldo(jogo.getPreco(), cliente.getSenha());
+    public int efetuaCompra(){
+        // A compra já foi realizada
+        if (cliente.getSaldoCarteira() != saldoOriginal){
+            return 2;
+        }
+        // Compra Realizada com sucesso
+        else if(cliente.subtraiSaldo(jogo.getPreco())){
+            return 0;
+        }
+        // Saldo insuficiente
+        else{
+            return 1;
+        }
     }
     
-    public void cancelaCompra(){
-        cliente.somaSaldo(jogo.getPreco(), cliente.getSenha());
+    public int cancelaCompra(){
+        // O estorno já foi realizado
+        if (cliente.getSaldoCarteira() == saldoOriginal){
+            return 2;
+        }
+        // Cancelamento da compra realizado com sucesso
+        else if(cliente.somaSaldo(jogo.getPreco())){
+            return 0;
+        }
+        // Preço de jogo invalido
+        else{
+            return 1;
+        }
     }
 }
